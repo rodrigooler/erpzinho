364ad2ed3d35aaa45e338c3dfd4f38dc0953d8c0
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2024-10-17 21:55:48 -0300

    refactor: update database column names and add timestamps

---
+++ api/prisma/migrations/20241018004809_/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `product_categories` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `product_categories` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "product_categories" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "product_category_products" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
